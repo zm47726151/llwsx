e898f621ebeff3674b746468138fa8d21a4b803c
zm47726151 <125651483@qq.com>
2014-11-09 00:01:05 +0800

    初始导入

---
+++ llwsx/src/main/java/com/thinkgem/jeesite/modules/sys/entity/Role.java
@@ -0,0 +1,206 @@
+/**
+ * Copyright &copy; 2012-2013 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ */
+package com.thinkgem.jeesite.modules.sys.entity;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.apache.commons.lang3.StringUtils;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
+import org.hibernate.annotations.Where;
+import org.hibernate.validator.constraints.Length;
+
+import com.google.common.collect.Lists;
+import com.thinkgem.jeesite.common.persistence.IdEntity;
+
+/**
+ * 角色Entity
+ * @author ThinkGem
+ * @version 2013-05-15
+ */
+@Entity
+@Table(name = "sys_role")
+@DynamicInsert @DynamicUpdate
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class Role extends IdEntity<Role> {
+	
+	private static final long serialVersionUID = 1L;
+	private String name; 	// 角色名称
+	private String dataScope; // 数据范围
+
+	private List<User> userList = Lists.newArrayList(); // 拥有用户列表
+	private List<Menu> menuList = Lists.newArrayList(); // 拥有菜单列表
+	public static final String DATA_SCOPE_ALL = "1";
+//	public static final String DATA_SCOPE_COMPANY_AND_CHILD = "2";
+//	public static final String DATA_SCOPE_COMPANY = "3";
+//	public static final String DATA_SCOPE_OFFICE_AND_CHILD = "4";
+//	public static final String DATA_SCOPE_OFFICE = "5";
+	public static final String DATA_SCOPE_SELF = "8";
+	public static final String DATA_SCOPE_CUSTOM = "9";
+	
+	public Role() {
+		super();
+		this.dataScope = DATA_SCOPE_CUSTOM;
+	}
+
+	public Role(String id, String name) {
+		this();
+		this.id = id;
+		this.name = name;
+	}
+	
+	@Length(min=1, max=100)
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDataScope() {
+		return dataScope;
+	}
+
+	public void setDataScope(String dataScope) {
+		this.dataScope = dataScope;
+	}
+
+	@ManyToMany(mappedBy = "roleList", fetch=FetchType.LAZY)
+	@Where(clause="del_flag='"+DEL_FLAG_NORMAL+"'")
+	@OrderBy("id") @Fetch(FetchMode.SUBSELECT)
+	@NotFound(action = NotFoundAction.IGNORE)
+	@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+	public List<User> getUserList() {
+		return userList;
+	}
+
+	public void setUserList(List<User> userList) {
+		this.userList = userList;
+	}
+	
+	@Transient
+	public List<String> getUserIdList() {
+		List<String> nameIdList = Lists.newArrayList();
+		for (User user : userList) {
+			nameIdList.add(user.getId());
+		}
+		return nameIdList;
+	}
+
+	@Transient
+	public String getUserIds() {
+		List<String> nameIdList = Lists.newArrayList();
+		for (User user : userList) {
+			nameIdList.add(user.getId());
+		}
+		return StringUtils.join(nameIdList, ",");
+	}
+
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "sys_role_menu", joinColumns = { @JoinColumn(name = "role_id") }, inverseJoinColumns = { @JoinColumn(name = "menu_id") })
+	@Where(clause="del_flag='"+DEL_FLAG_NORMAL+"'")
+	@OrderBy("id") @Fetch(FetchMode.SUBSELECT)
+	@NotFound(action = NotFoundAction.IGNORE)
+	@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+	public List<Menu> getMenuList() {
+		return menuList;
+	}
+
+	public void setMenuList(List<Menu> menuList) {
+		this.menuList = menuList;
+	}
+
+	@Transient
+	public List<String> getMenuIdList() {
+		List<String> menuIdList = Lists.newArrayList();
+		for (Menu menu : menuList) {
+			menuIdList.add(menu.getId());
+		}
+		return menuIdList;
+	}
+
+	@Transient
+	public void setMenuIdList(List<String> menuIdList) {
+		menuList = Lists.newArrayList();
+		for (String menuId : menuIdList) {
+			Menu menu = new Menu();
+			menu.setId(menuId);
+			menuList.add(menu);
+		}
+	}
+
+	@Transient
+	public String getMenuIds() {
+		List<String> nameIdList = Lists.newArrayList();
+		for (Menu menu : menuList) {
+			nameIdList.add(menu.getId());
+		}
+		return StringUtils.join(nameIdList, ",");
+	}
+	
+	@Transient
+	public void setMenuIds(String menuIds) {
+		menuList = Lists.newArrayList();
+		if (menuIds != null){
+			String[] ids = StringUtils.split(menuIds, ",");
+			for (String menuId : ids) {
+				Menu menu = new Menu();
+				menu.setId(menuId);
+				menuList.add(menu);
+			}
+		}
+	}
+	
+	
+	/**
+	 * 获取权限字符串列表
+	 */
+	@Transient
+	public List<String> getPermissions() {
+		List<String> permissions = Lists.newArrayList();
+		for (Menu menu : menuList) {
+			if (menu.getPermission()!=null && !"".equals(menu.getPermission())){
+				permissions.add(menu.getPermission());
+			}
+		}
+		return permissions;
+	}
+
+	@Transient
+	public boolean isAdmin(){
+		return isAdmin(this.id);
+	}
+	
+	@Transient
+	public static boolean isAdmin(String id){
+		return id != null && id.equals("1");
+	}
+	
+//	@Transient
+//	public String getMenuNames() {
+//		List<String> menuNameList = Lists.newArrayList();
+//		for (Menu menu : menuList) {
+//			menuNameList.add(menu.getName());
+//		}
+//		return StringUtils.join(menuNameList, ",");
+//	}
+}
